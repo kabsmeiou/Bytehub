1a72c3e257074fa39f106cef642133721d0fc73f
hopskiprabbit <94173937+kabsmeiou@users.noreply.github.com>
2024-05-09 21:51:13 +0800

    edited url paths

--- Bytehub/urls.py
+++ Bytehub/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
-    path('signin/', views.signin, name='signin'),
+    path('', views.signin, name='signin'),
     path('post/', views.post, name='post'),
     path('profile/', views.profile, name='profile'),
 ]
